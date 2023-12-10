588d7b3040e0462588b5f61b3187fb1a558cbc62
Amanmishra03 <aman274501mishra@gmail.com>
2023-12-10 21:56:19 +0530

    prep

--- 06.py
+++ 06.py
@@ -1,12 +1,12 @@
-print("Hello World")
-print(1+2)
-print(10*3)
-print(10*3.5)
-print()
-print("prepinsta")
-print("prepinsta is the best website ")
-print("prepinsta",  "is", "the", "best", "website",)
-print("prepinsta",  "is", "the", "best", "website", 5+10)
+# print("Hello World")
+# print(1+2)
+# print(10*3)
+# print(10*3.5)
+# print()
+# print("prepinsta")
+# print("prepinsta is the best website ")
+# print("prepinsta",  "is", "the", "best", "website",)
+# print("prepinsta",  "is", "the", "best", "website", 5+10)
 
 '''sep is used as a seperator in which we placed any symbol or character
  to sepetarte the sentences by writting the given symbol and  character. '''
